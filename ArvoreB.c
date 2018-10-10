aa871c1dca493c3a7770e79783c276a4bf713193
Gustavo Belfort <gustavobelfort@outlook.com>
2018-10-10 18:29:49 -0300

    Reverting to last Stable commit

--- ArvoreB.c
+++ ArvoreB.c
@@ -4,14 +4,15 @@
 #include <stdbool.h>
 #include<conio.h>
 
-int ORDEM = 2;
-int MAX_CHAVES = 2 * ORDEM - 1; //QUANTIDADE MÁXIMA DE CHAVES
-int MAX_FILHOS = 2 * ORDEM; // QUANTIDADE MÁXIMA DE FILHOS
-int MIN_OCUP = ORDEM - 1; // OCUPAÇÃO MINIMA EM CADA NÓ
+
+const ORDEM = 2;
+  MAX_CHAVES = 2 * ORDEM - 1; //QUANTIDADE MÁXIMA DE CHAVES
+  MAX_FILHOS = 2 * ORDEM; // QUANTIDADE MÁXIMA DE FILHOS
+  MIN_OCUP = ORDEM - 1; // OCUPAÇÃO MINIMA EM CADA NÓ
 
 /* Estrutura dos nós das árvores */
 typedef struct no_arvoreB {
-  struct no_arvoreB *arvoreB;
+  int isLeaf;
   int num_chaves; // quantidades de chaves contidas no nó
   int chaves[MAX_CHAVES]; //Chaves armazenadas no nó
   struct no_arvoreB *filhos[MAX_FILHOS]; //Ponteiro para filhos
@@ -50,6 +51,17 @@ int main(void){
   return 0;
 }
 
+no_arvoreB criarArvore(void)
+{
+  no_arvoreB raiz; 
+  b = malloc(sizeof(*raiz));
+  assert(raiz); 
+  b->isLeaf = 1;
+  b->num_chaves = MAX_CHAVES;
+  return b;
+}
+ 
+
 //insere uma chave e o ponteiro para o filho da direita em um nó
 void insere_chave(arvoreB *raiz,int info, arvoreB *filhodir)
 {
@@ -62,7 +74,7 @@ void insere_chave(arvoreB *raiz,int info, arvoreB *filhodir)
   //realiza o remanejamento para manter as chaves ordenadas
   while (k > pos && info < raiz->chaves[k-1])
   {
-    raiz->chaves[k] = raiz->[k-1];
+    raiz->chaves[k] = raiz->chaves[k-1];
     raiz->filhos[k+1] = raiz->filhos[k];
     k--;
   }
@@ -113,9 +125,87 @@ arvoreB *insere(arvoreB *raiz,int info,bool *h,int *info_retorno)
         raiz->chaves[i] = 0;
         raiz->filhos[i+1] = NULL;
       }
-      raiz-
+      raiz->num_chaves = MIN_OCUP;
+
+      //verifica em qual nó será inserida a nova chave
+      if (pos <= MIN_OCUP)
+        insere_chave(raiz, *info_retorno,filho_dir);
+      else insere_chave(temp,*info_retorno,filho_dir);
+
+      //retorna o mediano para inserí-lo no nó pai e o temp como filho direito do mediano.
+      *info_retorno = info_mediano;
+      return(temp);
     }
   }
+}
 
+arvoreB *insere_arvoreB(arvoreB *raiz,int info)
+{
+  bool h;
+  int info_retorno,i;
+  arvoreB *filho_dir,*nova_raiz;
 
+  filho_dir = insere(raiz,info,&h,&info_retorno);
+  if (h)
+  { //Aumentará a altura da àrvore
+  nova_raiz = (arvoreB *)malloc(sizeof(arvoreB));
+  nova_raiz->num_chaves = 1;
+  nova_raiz->chaves[0] = info_retorno;
+  nova_raiz->filhos[0] = raiz;
+  nova_raiz->filhos[1] = filho_dir;
+  for (i =2; i<= MAX_CHAVES; i++)
+    nova_raiz->filhos[i] = NULL;
+    return(nova_raiz);
+  }
+  else return(raiz);
+}
+
+int busca_binaria(arvoreB *no,int info){
+  int meio,i,f;
+  
+  i = 0;
+  f = no->num_chaves-1;
+
+  while (i <= f)
+  {
+    meio = (i + f) /2;
+    if(no->chaves[meio] == info)
+      return(meio); //encontrou. retorna a posição em que a chave está.
+    else if (no->chaves[meio] > info)
+      f = meio - 1;
+      else i = meio + 1;
+  }
+  return (i); // não encontrou. retorna a posição do ponteiro para o filho.
+
+}
+bool busca(arvoreB *raiz,int info)
+{
+  arvoreB *no;
+  int pos; //posição retornada pelo busca binaria.
+
+  no = raiz;
+  while (no != NULL)
+  {
+    pos = busca_binaria(no,info);
+    if (pos < no->num_chaves && no->chaves[pos] == info)
+      return(true);
+    else no = no->filhos[pos];
+  }
+  return(false);
+
+}
+
+//Algoritmo de varredura em ordem
+void em_ordem(arvoreB *raiz)
+{
+  int i;
+  if (raiz != NULL)
+  {
+    for (i = 0; i < raiz->num_chaves; i++)
+    {
+      em_ordem(raiz->filhos[i]);
+      printf("\n%d", raiz->chaves[i]);
+    }
+    em_ordem(raiz->filhos[i]);
+  }
 }
